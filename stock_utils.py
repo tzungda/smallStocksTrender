a9de28060279ac8480a0e1d6b02b36716d387bc0
tzungda <31838821+tzungda@users.noreply.github.com>
2022-11-05 19:17:19 -0700

    Update stock_utils.py

--- stock_utils.py
+++ stock_utils.py
@@ -1,18 +1,21 @@
 import yfinance as yf
 import pandas as pd
+import time
 
 class StockEarnings:
 
     def __init__(self, choice, num):
         self.__choice = choice# = input("Write a stock symbol: ")
         self.__num = num
+        self.__data = None
         
     def get_earnings_dates( self ):
         #self.__choice = input("Write a stock symbol: ")
-        data = yf.Ticker( self.__choice )
-        if data.earnings.empty:
+        self.__data = yf.Ticker( self.__choice )
+        if self.__data.earnings.empty:
             return pd.DataFrame()
-        e = data.earnings_dates[:self.__num]
+        e = self.__data.earnings_dates[:self.__num]
+        time.sleep(8)
         return e
         #
         #print (e)
